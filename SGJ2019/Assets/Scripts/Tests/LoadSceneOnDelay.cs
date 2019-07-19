d603359299aff711e185b125bc01e82b333d9cb3
Wooki <wooki1@op.pl>
2019-07-19 22:58:37 +0200

    Improvised tests

---
+++ SGJ2019/Assets/Scripts/Tests/LoadSceneOnDelay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace SGJ2019
+{
+	public class LoadSceneOnDelay : MonoBehaviour
+	{
+		[SerializeField] private float time = 1.0f;
+		[SerializeField] private string sceneName;
+
+
+		private void Update()
+		{
+			if (time >= 0.0f)
+			{
+				time -= Time.deltaTime;
+				if (time < 0.0f)
+				{
+					SceneManager.LoadScene(sceneName);
+					Debug.Log(sceneName + " loaded");
+				}
+			}
+		}
+	}
+}
